b41b9acf99780dd3db60a5fef64c5b705f9b385a
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2018-08-28 10:16:47 +0200

    App Manifest/Configuration can be edited in Generic Form-based Editor #2
    * Extending in JSon Schema in order to relate to Ecore more closely

--- smart-home-editor-extension/src/schema.ts
+++ smart-home-editor-extension/src/schema.ts
@@ -1,4 +1,123 @@
 export default {
+
+  definitions: {
+    // STATE
+    state: {
+      $id: "#state",
+      type: "object",
+      properties: {
+        id: { type: "string" },
+        description: { type: "string" },
+        name: { type: "string" },
+        multiple: { type: "boolean" },
+        requiresName: { type: "boolean" },
+        type: {
+          type: "string",
+          enum: [
+            "Item",
+            "Virtual",
+            "Framework"
+          ]
+        },
+        tags: {
+          type: "array",
+          items: {
+            type: "string"
+          }
+        },
+        defaultValueString: { type: "string" },
+        defaultValue: { type: "object" }
+      },
+      required: ["id", "description", "name", "multiple", "requiresName", "type", "tags", "defaultValueString", "defaultValue"]
+    },
+
+    // CONCRETE STATES
+    booleanState: {
+      $id: "#booleanState",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/state" }
+      ]
+    },
+    dateTimeState: {
+      $id: "#dateTimeState",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/state" }
+      ]
+    },
+    numberState: {
+      $id: "#numberState",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/state" }
+      ]
+    },
+
+    //CONCRETE ACTORS
+    heatingActor: {
+      $id: "#heatingActor",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/state" }
+      ]
+    },
+    lockUnlockActor: {
+      $id: "#lockUnlockActor",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/state" }
+      ]
+    },
+    onOffActor: {
+      $id: "#onOffActor",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/state" }
+      ]
+    },
+    playPauseActor: {
+      $id: "#playPauseActor",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/state" }
+      ]
+    },
+
+    // PARAMETERS
+    parameter: {
+      $id: "#parameter",
+      type: "object",
+      properties: {
+        name: { type: "string" }
+      },
+      required: ["name"]
+    },
+
+    // CONCRETE PARAMETERS
+    booleanParameter: {
+      $id: "#booleanParameter",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/parameter" }
+      ]
+    },
+    dateTimeParameter: {
+      $id: "#dateTimeParameter",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/parameter" }
+      ]
+    },
+    numberParameter: {
+      $id: "#numberParameter",
+      type: "object",
+      anyOf: [
+        { $ref: "#/definitions/parameter" }
+      ]
+    }
+  },
+
   $id: "#appdescription",
   type: "object",
   properties: {
@@ -25,172 +144,46 @@ export default {
     },
     providedState: {
       type: "array",
-      items: {
-        $id: "#providedStateItem",
-        type: "object",
-        properties: {
-          id: {
-            type: "string"
-          },
-          description: {
-            type: "string"
-          },
-          name: {
-            type: "string"
-          },
-          multiple: {
-            type: "boolean"
-          },
-          requiresName: {
-            type: "boolean"
-          },
-          type: {
-            type: "string",
-            enum: [
-              "Item",
-              "Virtual",
-              "Framework"
-            ]
-          },
-          tags: {
-            type: "array",
-            items: {
-              type: "string"
-            }
-          },
-          defaultValueString: {
-            type: "string"
-          }
-        },
-        additionalProperties: false,
-        required: [
-          "id",
-          "description",
-          "name",
-          "multiple",
-          "requiresName",
-          "type",
-          "defaultValueString"
+      items: { 
+        anyOf: [
+          { $ref: "#/definitions/booleanState" },
+          { $ref: "#/definitions/dateTimeState" },
+          { $ref: "#/definitions/numberState" }
         ]
-      }
+       }
     },
+
+    // required state with 
     requiredStates: {
       type: "array",
-      items: {
-        $id: "#requiredStatesItem",
-        type: "object",
-        properties: {
-          id: {
-            type: "string"
-          },
-          description: {
-            type: "string"
-          },
-          name: {
-            type: "string"
-          },
-          multiple: {
-            type: "boolean"
-          },
-          requiresName: {
-            type: "boolean"
-          },
-          type: {
-            type: "string",
-            enum: [
-              "Item",
-              "Virtual",
-              "Framework"
-            ]
-          },
-          tags: {
-            type: "array",
-            items: {
-              type: "string"
-            }
-          },
-          defaultValueString: {
-            type: "string"
-          }
-        },
-        additionalProperties: false,
-        required: [
-          "id",
-          "description",
-          "name",
-          "multiple",
-          "requiresName",
-          "type",
-          "defaultValueString"
+      items: { 
+        anyOf: [
+          { $ref: "#/definitions/booleanState" },
+          { $ref: "#/definitions/dateTimeState" },
+          { $ref: "#/definitions/numberState" }
         ]
-      }
+       }
     },
     requiredActors: {
       type: "array",
-      items: {
-        "$id": "#requiredActor",
-        type: "object",
-        properties: {
-          id: {
-            type: "string"
-          },
-          description: {
-            type: "string"
-          },
-          name: {
-            type: "string"
-          },
-          multiple: {
-            type: "boolean"
-          },
-          requiresName: {
-            type: "boolean"
-          },
-          type: {
-            type: "string",
-            enum: [
-              "Item",
-              "Virtual",
-              "Framework"
-            ]
-          },
-          tags: {
-            type: "array",
-            items: {
-              type: "string"
-            }
-          },
-          defaultValueString: {
-            type: "string"
-          }
-        },
-        additionalProperties: false,
-        required: [
-          "id",
-          "description",
-          "name",
-          "multiple",
-          "requiresName",
-          "type",
-          "defaultValueString"
+      items: { 
+        anyOf: [
+          { $ref: "#/definitions/heatingActor" },
+          { $ref: "#/definitions/lockUnlockActor" },
+          { $ref: "#/definitions/onOffActor" },
+          { $ref: "#/definitions/playPauseActor" }
         ]
-      }
+       }
     },
     requiredParameters: {
       type: "array",
-      items: {
-        "$id": "#requiredParameter",
-        type: "object",
-        properties: {
-          name: {
-            type: "string"
-          }
-        },
-        additionalProperties: false,
-        required: [
-          "name"
+      items: { 
+        anyOf: [
+          { $ref: "#/definitions/booleanParameter" },
+          { $ref: "#/definitions/dateTimeParameter" },
+          { $ref: "#/definitions/numberParameter" }
         ]
-      }
+       }
     },
     enabled: {
       type: "boolean"
